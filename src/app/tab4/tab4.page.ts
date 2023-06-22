dd5214338b2f60617f6a282cce3de32469431d11
aguilerafriasg461 <aguilerafriasg461@gmail.com>
2023-06-22 13:06:17 -0400

    hproducto

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { HproductoService } from '../servicios-backend/hproducto/hproducto.service';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab4',
   templateUrl: './tab4.page.html',
   styleUrls: ['./tab4.page.scss'],
 })
-export class Tab4Page implements OnInit {
+export class Tab4Page  {
+  public listHProducto = [];
+  public id = ""
+  public cantidad = ""
+  public idProducto = ""
+  public idCarritoCompra=""
+  public producto=""
+  public carritoCompra=""
+  public swGuardarCambios = false
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private hproductoservice: HproductoService) { }
+  public GetHProductos(){
+    this.hproductoservice.GetHProductos().subscribe({
+        next: (response: HttpResponse<any>) => {
+            this.listHProducto = response.body;
+            console.log(this.listHProducto)
+        },
+        error: (error: any) => {
+            console.log(error);
+        },
+        complete: () => {
+            console.log('complete - this.GetHProductos()');
+        },
+    });
   }
+
+ 
 
 }
